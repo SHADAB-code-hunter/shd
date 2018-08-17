68853a2cfb34e85bdc68405b5f4b842880faa193
Mohd Ahmad <mohd.ahmad@cheep.care>
2018-08-17 14:36:46 +0530

    commit2

---
+++ app/src/main/java/com/mohdahmad/myapplication2/MainActivity2.java
@@ -0,0 +1,27 @@
+package com.mohdahmad.myapplication2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+}
