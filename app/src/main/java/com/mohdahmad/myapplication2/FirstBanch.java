e068dcdd24441fe82429c91af25f0dccda7e68f7
Mohd Ahmad <mohd.ahmad@cheep.care>
2018-08-25 14:33:18 +0530

    log changes

--- app/src/main/java/com/mohdahmad/myapplication2/FirstBanch.java
+++ app/src/main/java/com/mohdahmad/myapplication2/FirstBanch.java
@@ -9,6 +9,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -31,6 +32,8 @@ public class FirstBanch extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Log.d("mergerlog","mernfklfnvkfv");
     }
 
     @Override
