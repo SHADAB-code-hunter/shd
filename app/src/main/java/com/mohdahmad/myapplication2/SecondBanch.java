c1a7fbb68e64870465fbc75c448139d4f60f7bb9
Mohd Ahmad <mohd.ahmad@cheep.care>
2018-08-25 16:15:01 +0530

    nthinbg

--- app/src/main/java/com/mohdahmad/myapplication2/SecondBanch.java
+++ app/src/main/java/com/mohdahmad/myapplication2/SecondBanch.java
@@ -21,8 +21,7 @@ public class SecondBanch extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
