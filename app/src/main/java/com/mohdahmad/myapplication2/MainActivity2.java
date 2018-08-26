b3b8b7dbaaf22e7780b7027297fbae2a496292ec
Mohd Ahmad <mohd.ahmad@cheep.care>
2018-08-26 23:30:21 +0530

    commit master

--- app/src/main/java/com/mohdahmad/myapplication2/MainActivity2.java
+++ app/src/main/java/com/mohdahmad/myapplication2/MainActivity2.java
@@ -26,5 +26,6 @@ public class MainActivity2 extends AppCompatActivity {
         }).start();
         View view=this.findViewById(R.id.action_settings);
         View view2=this.findViewById(R.id.action_settings);
+        View view3=this.findViewById(R.id.action_settings);
     }
 }
